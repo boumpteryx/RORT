7109f0e49a324bbff13ce1c4b4f1bf72f04f9364
Antoine Desjardins <antoine.desjardins@eleves.enpc.fr>
2022-02-01 14:58:52 +0100

    MyFileName

--- main.jl
+++ main.jl
@@ -4,7 +4,7 @@ include("parser.jl")
 
 
 function myModel(MyFileName::String)
-  cout_ouverture, Fct_commod, func_cost, func_capacity, nb_nodes, nb_arcs, nb_commodities, latency, nb_functions_per_node, commodity, nb_func, exclusion = read_instance("test")
+  cout_ouverture, Fct_commod, func_cost, func_capacity, nb_nodes, nb_arcs, nb_commodities, latency, nb_functions_per_node, commodity, nb_func, exclusion = read_instance(MyFileName)
 
   # Create the model
   m = Model(CPLEX.Optimizer)
