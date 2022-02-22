b1b11d0de25893076f2ef94e018b22db69f7efb8
Victor <victorspitzer@gmail.com>
2022-02-22 18:25:27 +0100

    Update

--- relaxed.jl
+++ relaxed.jl
@@ -29,37 +29,42 @@ function Relaxed(fileName :: String)
   @constraint(m, [i in 1:nb_nodes, k in 1:nb_commodities, f in 1:nb_func], x_ikf[i,k,f] >= 0) # RELAXE
   @constraint(m, [i in 1:nb_nodes, j in 1:nb_nodes, k in 1:nb_commodities, f in 1:nb_func+1], e[i,j,k,f] <= 1) # RELAXE
 
-
 	for i in 1:nb_nodes
-		@constraint(m,[f in 1:nb_func],x_fi[f,i]*func_capacity[f] >= sum(x_ikf[i,k,f]*commodity[k,3] for k in 1:nb_commodities)) #Nombre de fonctions f à placer en i
-		@constraint(m,sum(x_fi[f,i] for f in 1:nb_func) <= node_capacity[i] ) #capacité de noeud
+		@constraint(m,[f in 1:nb_func],x_fi[f,i]*func_capacity[f] >= sum(x_ikf[i,k,f]*commodity[k,3] for k in 1:nb_commodities), base_name = "n_"*string(f)*"_"*string(i)) #Nombre de fonctions f à placer en i
+		@constraint(m,sum(x_fi[f,i] for f in 1:nb_func) <= node_capacity[i], base_name = "cap_"*string(i) ) #capacité de noeud
 		for k in 1:nb_commodities
-			@constraint(m,[f in 1:nb_func],x_i[i] >= x_ikf[i,k,f]) #ouverture du noeud en i
-			@constraint(m,[f in 1:nb_func],x_ikf[i,k,f] <= length( findall( y -> y == f, Fct_commod[i] ))) #fixer à 0 x_ikf si fonction f n'est pas à appliquer à k
-			@constraint(m,[(v,w) in exclusion[k]], x_ikf[i,k,v]+x_ikf[i,k,w] <= 1) #exclusion
+			@constraint(m,[f in 1:nb_func],x_i[i] >= x_ikf[i,k,f], base_name = "ouverture_"*string(i)*"_"*string(f)) #ouverture du noeud en i
+			if size(exclusion[k,:])[1]>0
+				@constraint(m,sum(x_ikf[i,k,w] for w in exclusion[k,:]) <= 1, base_name = "exclusion_"*string(k)*"_"*string(i)) #exclusion
+			end				
 			for j in 1:nb_nodes
 				if latency[i,j] == 0
-					@constraint(m,[f in 1:nb_func],e[i,j,k,f]==0) #absence d'arcs
+					@constraint(m,[f in 1:nb_func],e[i,j,k,f]==0, base_name = "no_arc_"*string(i)*"_"*string(j)*"_"*string(f)) #absence d'arcs
 				end
 			end
 		end
 	end
-
-	for k in 1:nb_commodities
-		for f in 1:nb_func
-			@constraint(m,sum(x_ikt[i,k,f] for i in 1:nb_nodes)==1) #concatenation des problemes de flot
-		end
-	end
-
+	
+	
 	#Contrainte de flots successifs pour chaque commodité
 	for k in 1:nb_commodities
-		@constraint(m,sum(e[commodity[k,1],j,k,Fct_commod[k,1]] - e[j,commodity[k,1],k,Fct_commod[k,1]] for j in 1:nb_nodes)-x_ikf[commodity[k,1],k,Fct_commod[k,1]]==1) #initialisation du flot à la source
-		@constraint(m,[i in 1:nb_nodes ; i!=commodity[k,1]],sum(e[i,j,k,Fct_commod[k,1]] - e[j,i,k,Fct_commod[k,1]]  for j in 1:nb_nodes)+x_ikf[i,k,Fct_commod[k,1]]==0) #flot pour traiter la première fonction de puis la source
-		for t in 2:tab[k]
-			@constraint(m,[i in 1:nb_nodes],sum(e[i,j,k,Fct_commod[k,t]] - e[j,i,k,Fct_commod[k,t]] for j in 1:nb_nodes)-x_ikf[i,k,Fct_commod[k,t-1]]+x_ikf[i,k,Fct_commod[k,t]]==0) #flot d'un traitement au suivant
+		size_fk=length( findall( y -> y > 0, Fct_commod[k,:]))
+		
+		tab_fk=sortperm( Fct_commod[k,:])[1:size_fk]
+
+		@constraint(m,sum(e[commodity[k,1],j,k,tab_fk[1]] - e[j,commodity[k,1],k,tab_fk[1]] for j in 1:nb_nodes)-x_ikf[commodity[k,1],k,tab_fk[1]]==1, base_name = "init_flot_"*string(k)) #initialisation du flot à la source
+		@constraint(m,[i in 1:nb_nodes ; i!=commodity[k,1]],sum(e[i,j,k,tab_fk[1]] - e[j,i,k,tab_fk[1]]  for j in 1:nb_nodes)+x_ikf[i,k,tab_fk[1]]==0, base_name = "1er_flot_"*string(k)*"_"*string(i)) #flot pour traiter la première fonction de puis la source
+		for t in 2:size_fk
+			@constraint(m,[i in 1:nb_nodes],sum(e[i,j,k,tab_fk[t]] - e[j,i,k,tab_fk[t]] for j in 1:nb_nodes)-x_ikf[i,k,tab_fk[t-1]]+x_ikf[i,k,tab_fk[t]]==0, base_name = "mid_flow_"*string(t)*"_"*string(k)*"_"*string(i)) #flot d'un traitement au suivant
 		end
-		@constraint(m,[i in 1:nb_nodes ; i!=commodity[k,2]],sum(e[i,j,k,Fct_commod[k,end]] - e[j,i,k,Fct_commod[k,end]] for j in 1:nb_nodes)-x_ikf[i,k,Fct_commod[k,end]]==0) #flot du dernier traitement vers le puit
-		@constraint(m,sum(e[j,commodity[k,2],k,end] for j in 1:nb_nodes)+x_ikf[commodity[k,2],k,Fct_commod[k,end]]==1) #fin de flot sur le puit
+		@constraint(m,[i in 1:nb_nodes ; i!=commodity[k,2]],sum(e[i,j,k,nb_func+1] - e[j,i,k,nb_func+1] for j in 1:nb_nodes)-x_ikf[i,k,tab_fk[end]]==0, base_name = "last_flow_"*string(k)*"_"*string(i)) #flot du dernier traitement vers le puit
+		@constraint(m,sum(e[j,commodity[k,2],k,end] for j in 1:nb_nodes)+x_ikf[commodity[k,2],k,tab_fk[end]]==1, base_name = "end_flow_"*string(k)) #fin de flot sur le puit
+	end
+	
+	for k in 1:nb_commodities
+		for f in 1:nb_func
+			@constraint(m,sum(x_ikf[i,k,f] for i in 1:nb_nodes)<=1, base_name = "concatenation_"*string(k)*"_"*string(f)) #concatenation des problemes de flot
+		end
 	end
 
 	#Contraintes de latence
